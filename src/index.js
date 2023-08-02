3be74f7e1b7bacc0e4eaf76f7ea442f0d7bfe8c4
Mandeep Singh <mandeepgill039@gmail.com>
2023-08-02 15:57:38 -0400

    vercel analytics package added.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.js';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.js";
 
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
+import { inject } from "@vercel/analytics";
+
+inject();
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
